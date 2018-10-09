4c0e3aefed6b3ee18bd388cc9a4b95d5613ffa3f
f.konokhov <f.konokhov@partner.samsung.com>
2018-10-09 20:51:09 +0300

    add step-by-step

--- munk.py
+++ munk.py
@@ -1,3 +1,4 @@
+import numpy as np
 from munkres import Munkres, print_matrix
 matrix = [[11,4,11,6,11],
           [7,5,6,7,12],
@@ -13,3 +14,17 @@ for row, column in indexes:
     total += value
     print('(%d, %d) -> %d' % (row, column, value))
 print('total cost: %d' % total)
+print('-------------now step by step------------------')
+print('-------------minus in column ------------------')
+matrix = np.array([[11,4,11,6,11],
+          [7,5,6,7,12],
+          [9,7,8,10,10],
+          [9,11,6,10,9],
+          [7,10,4,8,8]])
+for i in range(5):
+    matrix[:,i] = (np.transpose(matrix[:,i] - min(matrix[:,i])))
+print(matrix)
+print('-------------minus in raw ------------------')
+for i in range(5):
+    matrix[i,:] = (np.transpose(matrix[i,:] - min(matrix[i,:])))
+print(matrix)
